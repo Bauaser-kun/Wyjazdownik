cfe88919610af161ee1071441794ba15d36a5e81
Bauaser-kun <michal.bala.1980@gmail.com>
2025-06-12 21:58:51 +0200

    update before permissions fix

--- app/src/main/java/com/example/wyjazdownik/MainActivity.java
+++ app/src/main/java/com/example/wyjazdownik/MainActivity.java
@@ -1,32 +1,60 @@
 package com.example.wyjazdownik;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView.findViewById(R.id.list);
+        items = new ArrayList<>();
+        items.add("something");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = items.get(i);
+                makeToast(name);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                makeToast("Przytrzymano dłużej na:" + items.get(i));
+                return false;
+            }
+        });
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+    }
+
+    Toast t;
+
+    private void makeToast(String s) {
+        if (t != null) {
+            t.cancel();
+        }
+
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
     }
 }
